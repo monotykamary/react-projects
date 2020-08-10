93c69a538d1827f0ada34540bb7d137f07719dc4
Nam <namtrhg@gmail.com>
2020-08-10 13:50:56 +0700

    something new

--- vananh/client/src/reducers/index.js
+++ vananh/client/src/reducers/index.js
@@ -1,5 +1,10 @@
 import {combineReducers} from "redux"
 import { tourListReducer, tourDetailsReducer, tourSavesReducer } from "./tourReducer"
+import Cookie from 'js-cookie';
+
+
+const userInfo = Cookie.getJSON('userInfo') || null;
+
 
 const rootReducer=combineReducers({
     tourSave: tourSavesReducer,
