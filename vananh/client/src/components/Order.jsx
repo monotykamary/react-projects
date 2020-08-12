8961f64682ae40d56b81952d567358422279c374
Nam <namtrhg@gmail.com>
2020-08-12 12:28:34 +0700

    fix iframe func

--- vananh/client/src/components/Order.jsx
+++ vananh/client/src/components/Order.jsx
@@ -1,35 +1,23 @@
 import React, { useEffect } from 'react';
 import Carousel from './Carousel';
 import Iframe from 'react-iframe'
-import {Helmet} from 'react-helmet'
-import ScriptTag from 'react-script-tag';
-import { useScript } from "./useScripts"
 
 function Order () {
-    const [loaded, error] = useScript("https://github.com/namtrhg/testing/blob/master/iframeResizer.js")
-
-    React.useEffect(() => {
-        if (!loaded) return
-    }, [loaded, error])
-
-
     return (
         <div>
             <Carousel/>
             <head>
-                <title>Đặt vé tàu cao tốc Phú quốc Express </title>
+                <title>Đặt vé tàu cao tốc Phú quốc Express</title>
             </head>
             <body>
-            {loaded && !error ?
-                <Iframe url="https://test.pqe.com.vn?iframe=1"
+                <Iframe url="https://test.pqe.com.vn/home"
                 width="100%"
-                height="600px"
+                height="400px"
                 id="myId"
                 className="myClassname"
                 display="initial"
                 scrolling
                 position="relative"/>
-                : <b>Something went wrong!</b>}
             </body>     
         </div>
     );
