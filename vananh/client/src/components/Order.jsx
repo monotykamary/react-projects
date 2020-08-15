21d62b6d5be06b24bfc821d034ffdd60fc270c33
Nam <namtrhg@gmail.com>
2020-08-15 10:01:00 +0700

    fix iframe post Message error by checking origin first

--- vananh/client/src/components/Order.jsx
+++ vananh/client/src/components/Order.jsx
@@ -1,25 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Carousel2 from './Carousel2'
 import IframeResizer from 'iframe-resizer-react'
 import "../App.css";
 import Footer from './Footer';
 import Contact from './Contact';
+import MessageData from './message-data'
+
 
 function Order () {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://raw.githubusercontent.com/davidjbradshaw/iframe-resizer/master/js/iframeResizer.min.js";
+        script.async = true;
+        document.body.appendChild(script);
+      return () => {
+          document.body.removeChild(script);
+        }
+      }, []);
+      
     return (
         <div className="order">
-            <div>
-                <h1>Đặt vé Du Lịch Vân Anh</h1>
-            </div>
-            <div>
-            <IframeResizer
-                log
-                src="https://www.pqe.com.vn?iframe=1"
-                style={{ width: '1px', minWidth: '100%'}}
-                />
-            </div>
-            <Contact/>
-            <Footer/>    
+            <>
+      <IframeResizer
+        heightCalculationMethod="lowestElement"
+        inPageLinks
+        log
+        checkOrigin="https://test.pqe.com.vn/home"
+        src="https://test.pqe.com.vn/home"
+        style={{ width: '1px', minWidth: '100%'}}
+      />
+    </>
         </div>
     );
 }
